50ae6647e0317d7cff5dd5ab2abf5606b9469079
JustinCummings36 <83243616+JustinCummings36@users.noreply.github.com>
2022-01-04 14:14:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function ButterflyWingSpeed(){
+var Question = prompt("do you like butterflys");
+if(Question == "yes" || Question == "Yes" || Question == "y"){
+alert("thats because your a bad ass. ");
+alert("Congradulations!!! you have joined the cult of the butterfly.");
+alert("heres a cool fact. During powerd or flapping flight, monarchs flap their wings about 5 to 12 times a second, depending on how hard they're trying to move. They flap at the slower rate when flying leisurely, such as during migration.");
+}
+if(Question =="no" || Question == "No" || Question == "n"){
+alert("fuck off then you cunt.");
+alert("you have been rejected by the cult of the butterfly");
+}
+}
