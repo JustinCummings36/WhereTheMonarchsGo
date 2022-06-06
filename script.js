24e2707d80b852f8e3088731774caea8c4d66387
JustinCummings36 <83243616+JustinCummings36@users.noreply.github.com>
2022-06-05 21:32:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ alert("Congradulations!!! you have joined the cult of the butterfly.");
 alert("heres a cool fact. During powerd or flapping flight, monarchs flap their wings about 5 to 12 times a second, depending on how hard they're trying to move. They flap at the slower rate when flying leisurely, such as during migration.");
 }
 if(Question =="no" || Question == "No" || Question == "n"){
-alert("fuck off then you cunt.");
+alert(" D: ");
 alert("you have been rejected by the cult of the butterfly");
 }
 }
